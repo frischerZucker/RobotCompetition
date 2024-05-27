a4e38090841bc5a2ce29e4144cc65198d48ac438
moritz <62518490+frischerZucker@users.noreply.github.com>
2024-05-27 09:55:11 +0200

    initial commit

---
+++ main.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.c
+ * Author: moritz
+ *
+ * Created on May 27, 2024, 9:50 AM
+ */
+
+#include <avr/io.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    while(1){
+        
+    }
+}
+
